b564373dfeb286ea52cff2e49bd29c325396bdd8
Link4Electronics <link4electronics@gmail.com>
2026-01-25 21:36:26 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,8 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/opentyrian2000
-mv ./AppDir/share/opentyrian2000/* ./AppDir/shared/bin
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/shared/bin' >> ./AppDir/.env
+#mv ./AppDir/share/opentyrian2000/* ./AppDir/shared/bin
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/opentyrian2000' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
