1796c92b70b29630e047705dba5867ff0e202c62
Link4Electronics <link4electronics@gmail.com>
2026-02-18 23:27:32 -0300

    add WMCLASS

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/opentyrian2000.png
 export DESKTOP=/usr/share/applications/opentyrian2000.desktop
+export STARTUPWMCLASS=opentyrian2000
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
