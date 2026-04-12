c312defb3e4382ccadb36a973fd68c9bf0fcbfda
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:43:36 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,21 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q opentyrian2000 | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/opentyrian2000.png
-export DESKTOP=/usr/share/applications/opentyrian2000.desktop
+export ICON=/usr/local/share/icons/hicolor/128x128/apps/opentyrian2000.png
+export DESKTOP=/usr/local/share/applications/opentyrian2000.desktop
 export STARTUPWMCLASS=opentyrian2000
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/opentyrian2000
+quick-sharun /usr/local/bin/opentyrian2000
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/opentyrian2000' >> ./AppDir/.env
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
