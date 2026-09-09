5b8f7d2f7b9bc10008a00c646e6d732acd4c9f3e
Link4Electronics <link4electronics@gmail.com>
2026-09-09 01:18:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,8 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/opentyrian2000.png
-export DESKTOP=/usr/share/applications/opentyrian2000.desktop
+export ICON=https://raw.githubusercontent.com/KScl/opentyrian2000/refs/heads/master/linux/icons/tyrian2000-128.png
+export DESKTOP=https://raw.githubusercontent.com/KScl/opentyrian2000/refs/heads/master/linux/opentyrian2000.desktop
 export STARTUPWMCLASS=opentyrian2000
 export DEPLOY_OPENGL=1
 
