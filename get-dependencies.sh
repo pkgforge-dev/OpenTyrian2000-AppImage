1d9e7fba0069271e5efd5d70de10856ba4503f9a
Link4Electronics <link4electronics@gmail.com>
2026-09-09 01:14:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,12 +26,11 @@ echo "$VERSION" > ~/version
 mkdir -p /usr/share/opentyrian2000
 wget https://camanis.net/tyrian/tyrian2000.zip
 bsdtar -xvf ./tyrian2000.zip \
-  -C /usr/share/opentyrian2000 \
-  -s '/^tyrian2000\///' \
-  'tyrian2000/*.dat' 'tyrian2000/*.lvl' 'tyrian2000/*.shp' 'tyrian2000/*.snd' \
-  'tyrian2000/demo.*' 'tyrian2000/music.mus' 'tyrian2000/tyrend.anm' \
-  'tyrian2000/tyrian.cdt' 'tyrian2000/tyrian.hdt' 'tyrian2000/tyrian.pic' \
-  'tyrian2000/tshp2.pcx'
+  -s "/^tyrian2000//" \
+  "tyrian2000/*.dat" "tyrian2000/*.lvl" "tyrian2000/*.shp" "tyrian2000/*.snd" \
+  "tyrian2000/demo.*" "tyrian2000/music.mus" "tyrian2000/tyrend.anm" \
+  "tyrian2000/tyrian.cdt" "tyrian2000/tyrian.hdt" "tyrian2000/tyrian.pic" \
+  "tyrian2000/tshp2.pcx"
 cd ./opentyrian2000
 make prefix=/usr gamesdir=/usr/share all -j$(nproc)
 make install
