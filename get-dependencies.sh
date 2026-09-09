d0c5ad8e4ffed2dd894119da494b98a8e96f8b18
Link4Electronics <link4electronics@gmail.com>
2026-09-09 00:58:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,22 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm hicolor-icon-theme sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
-make-aur-package opentyrian2000
+#make-aur-package opentyrian2000
 
 # If the application needs to be manually built that has to be done down here
+echo "Building OpenTyrian2000..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/KScl/opentyrian2000"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./opentyrian2000
+echo "$VERSION" > ~/version
+
+wget https://camanis.net/tyrian/tyrian2000.zip
+make prefix=/usr gamesdir=/usr/share all -j$(nproc)
