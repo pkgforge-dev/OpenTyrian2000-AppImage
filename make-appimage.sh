1b68107d7e582af9322df7d7421b1a282c54e4b5
Link4Electronics <link4electronics@gmail.com>
2026-09-09 01:26:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,13 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=https://raw.githubusercontent.com/KScl/opentyrian2000/refs/heads/master/linux/icons/tyrian2000-128.png
-export DESKTOP=https://raw.githubusercontent.com/KScl/opentyrian2000/refs/heads/master/linux/opentyrian2000.desktop
+export ICON=/usr/local/share/icons/hicolor/128x128/apps/opentyrian2000.png
+export DESKTOP=/usr/local/share/applications/opentyrian2000.desktop
 export STARTUPWMCLASS=opentyrian2000
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/opentyrian2000
+quick-sharun /usr/local/bin/opentyrian2000
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/opentyrian2000' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
